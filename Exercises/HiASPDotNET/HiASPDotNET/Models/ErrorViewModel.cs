05cf74f5928c3492c9e8aa54d9a5a7a4143f7fda
Paul <greatdogsite@gmail.com>
2024-01-23 15:13:03 -0600

    Chapter Nine Completed

---
+++ Exercises/HiASPDotNET/HiASPDotNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HiASPDotNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
