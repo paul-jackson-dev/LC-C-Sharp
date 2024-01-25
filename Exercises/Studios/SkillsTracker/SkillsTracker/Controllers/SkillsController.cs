a146ebbb8f8be61384443ca5c718471efeac65cd
Paul <greatdogsite@gmail.com>
2024-01-25 17:01:08 -0600

    Chapter Ten Completed

---
+++ Exercises/Studios/SkillsTracker/SkillsTracker/Controllers/SkillsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkillsTracker.Controllers
+{
+    [Route("skills")]
+    public class SkillsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<h1> Skills Tracker</h1>" +
+                "<ol> <li>Java</li><li>C#</li><li>Python</li></ol>";
+            return Content(html, "text/html");
+        }
+
+        //GET: skills/form
+        [HttpGet("form")]
+        public IActionResult DisplayForm()
+        {
+            string html = "<h1>What is your favorite programming language?</h1><form method='post' action='/skills/form'>" +
+   "<input type='text' name='language' />" +
+   "<input type='submit' value='My Favorite!' />" +
+   "</form>";
+            return Content(html, "text/html");
+        }
+
+        //POST: /skills/form
+        [HttpPost("form")]
+        public IActionResult RenderForm(string language)
+        {
+            string html = "You chose " + language;
+            return Content(html, "text/html");
+        }
+    }
+}
---
+++ Exercises/Studios/SkillsTracker/SkillsTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SkillsTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
