cef7c0ca04e7c3b3efbf37ea127e5342b2297c8e
Paul <greatdogsite@gmail.com>
2024-01-30 14:28:34 -0600

    Chapter Thirteen Complete

---
+++ Exercises/CodingEvents/CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+    [Route("event")]
+    public class EventsController : Controller
+    {
+        //private static List<string> Events = new List<string>(); //{ "Tom's Event", "Danny's Event", "Mark's Event" };
+        private static Dictionary<string, string> Events = new Dictionary<string, string>();
+
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            //List<string> Events = new List<string>() { "Tom's Event", "Danny's Event", "Mark's Event" };
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        [Route("add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult NewEvent(string name, string description)
+        {
+            Events.Add(name, description);
+
+            return Redirect("/event");
+        }
+    }
+}
--- Exercises/HelloASPDotNET-main/HelloASPDotNET/Controllers/HelloController.cs
+++ Exercises/HelloASPDotNET-main/HelloASPDotNET/Controllers/HelloController.cs
@@ -2,7 +2,7 @@
 
 namespace HelloASPDotNET.Controllers
 {
-    //[Route("")]
+    [Route("")]
     public class HelloController : Controller
     {
         // GET: /hello/
@@ -11,17 +11,17 @@ namespace HelloASPDotNET.Controllers
         public IActionResult Index()
         {
 
-            string html = "<form method='post' action = 'hello'>" +
-                "<input type='text' name='name' />" +
-                "<select name='language'><option value='english' selected>English</option>" +
-                "<option value='spanish'>Spanish</spanish>" +
-                "<option value='bosnian'>Bosnian</option>" +
-                "<option value='vietnamese'>Vietnamese</option>" +
-                "<option value='french'>French</option></select>" +
-                "<input type='submit' value='Greet Me!'/>" +
-                "</form>";
+            //string html = "<form method='post' action = 'hello'>" +
+            //    "<input type='text' name='name' />" +
+            //    "<select name='language'><option value='english' selected>English</option>" +
+            //    "<option value='spanish'>Spanish</spanish>" +
+            //    "<option value='bosnian'>Bosnian</option>" +
+            //    "<option value='vietnamese'>Vietnamese</option>" +
+            //    "<option value='french'>French</option></select>" +
+            //    "<input type='submit' value='Greet Me!'/>" +
+            //    "</form>";
 
-            return Content(html, "text/html");
+            return View();
         }
 
         //// GET : /<controller>/welcome
@@ -37,11 +37,12 @@ namespace HelloASPDotNET.Controllers
         [HttpPost] // responds to post request at /hello
         [Route("/hello")]
         //[HttpGet("welcome/{name?}")] // responds to get request at /hello/welcome/{name}
-        public IActionResult WelcomePost(string name = "no name given", string language = "english")
+        public IActionResult Welcome(string name = "no name given", string language = "english")
         {
             language = char.ToUpper(language[0]) + language.Substring(1); // convert first letter to Uppercase
-            string html = "<h1>" + "Hi! " + name + " in " + language + "</ h1 > ";
-            return Content(html, "text/html");
+            ViewBag.name = name;
+            ViewBag.language = language;
+            return View();
         }
     }
 }
