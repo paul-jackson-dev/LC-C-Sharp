d9f952641c6afad89af53d8d3cc4c4ddeaa30992
Paul <greatdogsite@gmail.com>
2024-02-01 15:27:44 -0600

    Chapter Fourteen Complete

--- Exercises/CodingEvents/CodingEvents/Controllers/EventsController.cs
+++ Exercises/CodingEvents/CodingEvents/Controllers/EventsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodingEvents.Controllers
 {
@@ -6,15 +8,15 @@ namespace CodingEvents.Controllers
     public class EventsController : Controller
     {
         //private static List<string> Events = new List<string>(); //{ "Tom's Event", "Danny's Event", "Mark's Event" };
-        private static Dictionary<string, string> Events = new Dictionary<string, string>();
-
+        //private static Dictionary<string, string> Events = new Dictionary<string, string>();
+        //static private List<Event> Events = new List<Event>();
 
         [HttpGet]
         [Route("")]
         public IActionResult Index()
         {
             //List<string> Events = new List<string>() { "Tom's Event", "Danny's Event", "Mark's Event" };
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
         }
@@ -28,10 +30,52 @@ namespace CodingEvents.Controllers
 
         [HttpPost]
         [Route("add")]
-        public IActionResult NewEvent(string name, string description)
+        public IActionResult NewEvent(Event freshEvent)
         {
-            Events.Add(name, description);
+            EventData.Add(freshEvent);
 
+            return Redirect("/event");
+        }
+
+        [HttpGet]
+        [Route("delete")]
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("delete")]
+        public IActionResult DeleteProcessing(int[] eventIds)
+        {
+            foreach (int i in eventIds)
+            {
+                EventData.Remove(i);
+            }
+
+            return Redirect("/event");
+        }
+
+        [HttpGet]
+        [Route("edit/{eventId}")]
+        public IActionResult Edit(int eventId)
+        {
+            Event singleEvent = EventData.GetById(eventId);
+            ViewBag.title = "Edit Event " + singleEvent.Name + "(id=" + singleEvent.Id.ToString() + ")";
+            ViewBag.singleEvent = singleEvent;
+
+            return View();
+        }
+
+        [HttpPost]
+        [Route("edit")]
+        public IActionResult EditProcessing(int eventId, string name, string description)
+        {
+            Event editEvent = EventData.GetById(eventId);
+            editEvent.Name = name;
+            editEvent.Description = description;
+            //EventData.Save(editEvent); // this would be needed if we were connected to a db, but in this case, we are just updating the object itself
             return Redirect("/event");
         }
     }
---
+++ Exercises/CodingEvents/CodingEvents/Data/EventData.cs
@@ -0,0 +1,31 @@
+﻿using CodingEvents.Models;
+
+namespace CodingEvents.Data
+{
+    public class EventData
+    {
+
+        //public static List<Event> Events = new List<Event>();
+        private static Dictionary<int, Event> Events = new Dictionary<int, Event>();
+
+        //EventData() { }
+
+        public static IEnumerable<Event> GetAll()
+        {
+            return Events.Values;
+        }
+
+        public static void Add(Event e) { Events.Add(e.Id, e); }
+
+        public static void Remove(int id) { Events.Remove(id); } // pass in id of an event from a form
+
+        public static void Save(Event e)
+        {
+            Events[e.Id].Name = e.Name;
+            Events[e.Id].Description = e.Description;
+        }
+
+        public static Event GetById(int id) { return Events[id]; }
+
+    }
+}
