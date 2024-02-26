a506dc2fe08002c6979ce46da4ac192ddab1df1f
Paul <greatdogsite@gmail.com>
2024-02-26 16:25:03 -0600

    transferring to laptop to finish outside

---
+++ Exercises/coding-events-api/CodingEventsAPI/Controllers/CodingEventsController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CodingEventsAPI.Data;
+using CodingEventsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CodingEventsAPI.Controllers {
+  [ApiController]
+  [Route(Entrypoint)]
+  public class CodingEventsController : ControllerBase {
+    public const string Entrypoint = "/api/events";
+
+    private readonly CodingEventsDbContext _dbContext;
+
+    public CodingEventsController(CodingEventsDbContext dbContext) {
+      _dbContext = dbContext;
+    }
+
+
+    [HttpGet]
+    [SwaggerOperation(
+      OperationId = "GetCodingEvents",
+      Summary = "Retrieve all Coding Events",
+      Description = "Publicly available"
+    )]
+    [SwaggerResponse(200, "List of public Coding Event data", Type = typeof(List<CodingEvent>))]
+    public ActionResult GetCodingEvents() => Ok(_dbContext.CodingEvents.ToList());
+
+    [HttpPost]
+    [SwaggerOperation(OperationId = "RegisterCodingEvent", Summary = "Create a new Coding Event")]
+    [SwaggerResponse(201, "Returns new Coding Event data", Type = typeof(CodingEvent))]
+    [SwaggerResponse(400, "Invalid or missing Coding Event data", Type = null)]
+    public ActionResult RegisterCodingEvent([FromBody] NewCodingEventDto newCodingEventDto) {
+      var codingEventEntry = _dbContext.CodingEvents.Add(new CodingEvent());
+      codingEventEntry.CurrentValues.SetValues(newCodingEventDto);
+      _dbContext.SaveChanges();
+
+      var newCodingEvent = codingEventEntry.Entity;
+
+      return CreatedAtAction(
+        nameof(GetCodingEvent),
+        new { codingEventId = newCodingEvent.Id },
+        newCodingEvent
+      );
+    }
+
+    [HttpGet]
+    [Route("{codingEventId}")]
+    [SwaggerOperation(OperationId = "GetCodingEvent", Summary = "Retrieve Coding Event data")]
+    [SwaggerResponse(200, "Complete Coding Event data", Type = typeof(CodingEvent))]
+    [SwaggerResponse(404, "Coding Event not found", Type = null)]
+    public ActionResult GetCodingEvent([FromRoute] long codingEventId) {
+      var codingEvent = _dbContext.CodingEvents.Find(codingEventId);
+      if (codingEvent == null) return NotFound();
+
+      return Ok(codingEvent);
+    }
+
+    [HttpDelete]
+    [Route("{codingEventId}")]
+    [SwaggerOperation(
+      OperationId = "CancelCodingEvent",
+      Summary = "Cancel (delete) a Coding Event"
+    )]
+    [ProducesResponseType(204)] // suppress default swagger 200 response code
+    [SwaggerResponse(204, "No content success", Type = null)]
+    public ActionResult CancelCodingEvent([FromRoute] long codingEventId) {
+      _dbContext.CodingEvents.Remove(new CodingEvent { Id = codingEventId });
+
+      try {
+        _dbContext.SaveChanges();
+      }
+      catch (DbUpdateConcurrencyException) {
+        // row did not exist
+        return NotFound();
+      }
+
+      return NoContent();
+    }
+  }
+}
---
+++ Exercises/coding-events-api/CodingEventsAPI/Startup.cs
@@ -0,0 +1,65 @@
+using CodingEventsAPI.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace CodingEventsAPI {
+  public class Startup {
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services) {
+      services.AddControllers();
+      
+      services.AddDbContext<CodingEventsDbContext>(o => o.UseSqlite("Data Source=sqlite.db;"));
+      
+      services.AddSwaggerGen(
+        options => {
+          options.SwaggerDoc(
+            "v1",
+            new OpenApiInfo {
+              Version = "v1",
+              Title = "Coding Events API",
+              Description = "REST API for managing Coding Events"
+            }
+          );
+        }
+      );
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+      if (env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+      app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+      app.UseSwagger();
+      app.UseSwaggerUI(
+        options => {
+          options.RoutePrefix = ""; // root path of the server
+          options.SwaggerEndpoint(
+            "/swagger/v1/swagger.json",
+            "Patrick's Coding Events API Documentation"
+          );
+        }
+      );
+
+      // run migrations on startup
+      var dbContext = app.ApplicationServices.CreateScope()
+        .ServiceProvider.GetService<CodingEventsDbContext>();
+      dbContext.Database.Migrate();
+    }
+  }
+}
